9c0853807260405f8b40aa6e0a44fc15f88553c6
sjain.itp <sjain.itp@sparkcognition.com>
2023-01-27 02:56:47 +0530

    fix:testing git changes status

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,10 @@
 import Wrapper from "../assets/wrappers/Navbar";
-import {FaHome} from "react"
+import { FaHome } from "react";
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h4>Navbar</h4>
+      <h4>Navbar 123</h4>
     </Wrapper>
   );
 };
